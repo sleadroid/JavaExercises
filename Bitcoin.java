9e55f9e72dfe5261a1b4989166c40e40e9edaa5c
Niko Kivilahti <kivilahti.niko@gmail.com>
2023-01-04 22:13:37 +0200

    Update Bitcoin.java

--- Bitcoin.java
+++ Bitcoin.java
@@ -1,4 +1,4 @@
-package teht‰v‰;
+package teht√§v√§;
 import java.util.Scanner;
 
 public class Bitcoin {
@@ -7,11 +7,10 @@ public class Bitcoin {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("Anna Bitcoineihin investoidun rahan m‰‰r‰: ");	
+		System.out.println("Anna Bitcoineihin investoidun rahan m√§√§r√§: ");	
 	               
 		int bitcoin = sc.nextInt();
 		
-		// HUOM! Ohjeissa kerrottiin kertoimeksi 15,06 mutta itse teht‰v‰ haluaa kertoimeksi 14,06!
 		System.out.println("Bitcoin tuotti vuodessa " + (14.06 * bitcoin) + " euroa");
 
 	}
